1671263d610ab4447569fe24b53624d00db81aed
Aashish Kumar Singh <aashish.singh.17@mountblue.tech>
2021-08-18 16:49:58 +0530

    return cache

--- cacheFunction.js
+++ cacheFunction.js
@@ -2,9 +2,9 @@ function cacheFunction(cb) {
           var cache = new Object();
           function oneTime(a) {
                     if (!cache.hasOwnProperty(a)) { cache[a] = cb(a);
-                    console.log("CallBack Invoked");
+                    //console.log("CallBack Invoked");
                     return cb(a); }
-                    else { return cb(a); }
+                    else { return cache[a]; }
           }
           return oneTime;
 }
